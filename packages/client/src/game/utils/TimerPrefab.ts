4fe5bb869eec502cf9065d0c741ff9f3be9c4978
편오 <108973850+Editor0518@users.noreply.github.com>
2026-01-14 17:47:07 +0900

    feat: 타이머에 비율 먹임 #43

--- packages/client/src/game/utils/TimerPrefab.ts
+++ packages/client/src/game/utils/TimerPrefab.ts
@@ -5,30 +5,34 @@
 
 export default class TimerPrefab extends Phaser.GameObjects.Container {
 
-	constructor(scene: Phaser.Scene, x?: number, y?: number) {
-		super(scene, x ?? 0, y ?? 0);
+	   constructor(scene: Phaser.Scene, x?: number, y?: number) {
+		   const ratio = window.__APPLE_GAME_RATIO || 1;
+		   const clampedRatio = Math.min(Math.max(ratio, 0.5), 1.5); // 최소 0.5, 최대 1.0으로 클램핑
+		   super(scene, x ?? 0, y ?? 0);
 
-		// timerBar
-		const timerBar = scene.add.rectangle(0, 777, 22, 752);
-		timerBar.setOrigin(0.5, 1);
-		timerBar.isFilled = true;
-		timerBar.fillColor = 0x3fa425; // 시작 시 초록색
-		this.add(timerBar);
+		   // timerBar
+		   const timerBar = scene.add.rectangle(0, 777 * clampedRatio, 22 * clampedRatio, 752 * clampedRatio);
+		   timerBar.setOrigin(0.5, 1);
+		   timerBar.isFilled = true;
+		   timerBar.fillColor = 0x3fa425; // 시작 시 초록색
+		   timerBar.setScale(clampedRatio);
+		   this.add(timerBar);
 
-		// timeTxt
-		const timeTxt = scene.add.text(0, 0, "", {});
-		timeTxt.scaleX = 0.9;
-		timeTxt.scaleY = 1.3;
-		timeTxt.setOrigin(0.5, 0.5);
-		timeTxt.text = "TIME";
-		timeTxt.setStyle({ "color": "#000000ff", "fontSize": "32px", "fontStyle": "bold", "stroke": "#000000ff" });
-		this.add(timeTxt);
+		   // timeTxt
+		   const timeTxt = scene.add.text(0, 0, "", {});
+		   timeTxt.scaleX = 0.9 * clampedRatio;
+		   timeTxt.scaleY = 1.3 * clampedRatio;
+		   timeTxt.setResolution(5); // 폰트 좀더 선명하게
+		   timeTxt.setOrigin(0.5, 0.5);
+		   timeTxt.text = "TIME";
+		   timeTxt.setStyle({ "color": "#000000ff", "fontSize": `${32 * clampedRatio}px`, "fontStyle": "bold", "stroke": "#000000ff" });
+		   this.add(timeTxt);
 
-		/* START-USER-CTR-CODE */
-		// timerBar 참조 저장
-		this.timerBar = timerBar;
-		/* END-USER-CTR-CODE */
-	}
+		   /* START-USER-CTR-CODE */
+		   // timerBar 참조 저장
+		   this.timerBar = timerBar;
+		   /* END-USER-CTR-CODE */
+	   }
 
 	/* START-USER-CODE */
 
